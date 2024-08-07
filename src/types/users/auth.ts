21d8af296c261340cae52750b86ea051b6042c0e
= <=>
2024-08-07 16:52:04 +0700

    fix: type of user

--- src/types/users/auth.ts
+++ src/types/users/auth.ts
@@ -4,5 +4,7 @@ export type Role = 'admin' | 'author' | 'reader'
 
 export type UserLoginParams = Pick<User, 'email' | 'password'>
 
-export type UserRegisterParams = Pick<User, 'name' | 'email' | 'password' | 'avatar'>
+export type UserRegisterParams = Pick<User, 'name' | 'email' | 'password'> & {
+  avatar: string
+}
 export type UserAuthResponse = Omit<User, 'password'>
